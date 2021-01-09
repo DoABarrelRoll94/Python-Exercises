f7eef8b703e80a8d84dff79d78e902e7af5671ec
Mark-Horace Milaor <mark.milaor@gmail.com>
2021-01-09 16:04:36 -0500

    Update listLessThanTen.py

--- listLessThanTen.py
+++ listLessThanTen.py
@@ -1,3 +1,5 @@
+#exercise3
+
 import random
 import sys
 import os
@@ -10,16 +12,17 @@ new_list = []                                           # "new_list" will have l
 for elem in a:
     if elem < 5:
         new_list.append(elem)
-        
-print(new_list)
+
+print("List A:", a)       
+print("List of numbers in List A that are less than 5:", new_list)
 
 # 2
 
-print(list(filter(lambda x: x < 5, a)))                 # does same as "new_list" but in one line of code
+print("Same function as line above, but coded in one line:", list(filter(lambda x: x < 5, a)))                 # does same as "new_list" but in one line of code
 
 # 3
 
-x = int(input("Give me a number. "))
+x = int(input("\nGive me a number. I will print out a list of numbers from List A that are less than what you give me.\n"))
 
 def list_less_than_input(x):                            # function that creates and returns a list containing numbers less than input
     new_list = []
